a23653a916f1244da6fdbf6e7c5422e40e42c9de
alex <alex@alex.com>
2014-02-21 17:15:33 +0100

    ara si

---
+++ alexmola/src/alexmola/MolaMola.java
@@ -0,0 +1,11 @@
+package alexmola;
+
+public class  MolaMola {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.print("En realitat xavi es el que mola");
+	}
+
+}
